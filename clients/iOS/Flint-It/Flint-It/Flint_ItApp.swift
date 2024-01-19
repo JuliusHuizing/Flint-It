1bf97d614dc22b7780330cf8baea4082dc0b57e4
Julius <jbhuizing@gmail.com>
2024-01-19 15:40:47 +0100

    set up xcode project.

---
+++ clients/iOS/Flint-It/Flint-It/Flint_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flint_ItApp.swift
+//  Flint-It
+//
+//  Created by Julius Huizing on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Flint_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
